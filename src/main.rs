16998169a312f71d7d2c45be972bf6e666c9ebde
Dave <somerandonameddave@swisscows.email>
2023-07-29 21:44:02 +0100

    switch Results for Options and remove anyhow as it's not really being utilised

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ use std::{
     path::Path
 };
 
-use anyhow::{bail, Result};
 use chrono::prelude::*;
-use nvml_wrapper::{enum_wrappers::device::TemperatureSensor::Gpu, error::NvmlError, Nvml};
+use nvml_wrapper::{enum_wrappers::device::TemperatureSensor::Gpu, Nvml};
 
 const BQ: &str = "\x1b[0;38:5:19m!?\x1b[0m";
 const UNKNOWN: &str = "\x1b[0;38:5:19mUnknown!\x1b[0m";
@@ -34,8 +33,8 @@ fn main() {
     // disk usage
     // kernel
     let (npkg, mpkg) = match pkgs() {
-        Ok(x) => x,
-        Err(_) => (BQ.to_string(), BQ.to_string())
+        Some(x) => x,
+        None => (BQ.to_string(), BQ.to_string())
     };
     // uptime
 
@@ -74,20 +73,20 @@ fn main() {
         editor,
         browser,
         font,
-        wall().unwrap_or_else(|_| UNKNOWN.to_string()),
+        wall().unwrap_or_else(|| UNKNOWN.to_string()),
         mid,
-        ctemp().unwrap_or_else(|_| BQ.to_string()),
-        gtemp().unwrap_or_else(|_| BQ.to_string()),
-        ram().unwrap_or_else(|_| UNKNOWN.to_string()),
-        disk().unwrap_or_else(|e| e.to_string()),
-        kernel().unwrap_or_else(|e| e.to_string()),
+        ctemp().unwrap_or_else(|| BQ.to_string()),
+        gtemp().unwrap_or_else(|| BQ.to_string()),
+        ram().unwrap_or_else(|| UNKNOWN.to_string()),
+        disk().unwrap_or_else(|| UNKNOWN.to_string()),
+        kernel().unwrap_or_else(|| UNKNOWN.to_string()),
         npkg,
         mpkg,
-        uptime().unwrap_or_else(|_| UNKNOWN.to_string())
+        uptime().unwrap_or_else(|| UNKNOWN.to_string())
     );
 }
 
-fn wall() -> Result<String> {
+fn wall() -> Option<String> {
     let p = read_link(
         [
             &var("XDG_CONFIG_HOME")
@@ -97,77 +96,88 @@ fn wall() -> Result<String> {
             "wallpaper"
         ]
         .join("/")
-    )?;
+    )
+    .ok()?;
     let mut f = p.file_name().unwrap().to_string_lossy().to_string();
     match f.len() <= 28 {
-        true => Ok(f),
+        true => Some(f),
         false => {
             f.truncate(26);
-            Ok([f, "…".to_string()].join(""))
+            Some([f, "…".to_string()].join(""))
         }
     }
 }
 
-fn ctemp() -> Result<String> {
-    let input = read_to_string("/sys/devices/platform/coretemp.0/hwmon/hwmon2/temp1_input")?;
-    let temp = input.trim().parse::<i32>()?;
-    Ok((temp as f32 / 1_000f32).to_string())
+fn ctemp() -> Option<String> {
+    let input = read_to_string("/sys/devices/platform/coretemp.0/hwmon/hwmon2/temp1_input").ok()?;
+    let temp = input.trim().parse::<i32>().ok()?;
+    Some((temp as f32 / 1_000f32).to_string())
 }
 
-fn gtemp() -> Result<String, NvmlError> {
-    let nvml = Nvml::init()?;
-    let device = nvml.device_by_index(0)?;
-    let temp = device.temperature(Gpu)?;
-    Ok(temp.to_string())
+fn gtemp() -> Option<String> {
+    let nvml = Nvml::init().ok()?;
+    let device = nvml.device_by_index(0).ok()?;
+    let temp = device.temperature(Gpu).ok()?;
+    Some(temp.to_string())
 }
 
-fn ram() -> Result<String> {
-    let mut lines = io::BufReader::new(File::open("/proc/meminfo")?).lines().take(26);
+fn ram() -> Option<String> {
+    let mut lines = io::BufReader::new(File::open("/proc/meminfo").ok()?).lines().take(26);
 
     let total = lines
         .next()
-        .unwrap()?
+        .unwrap()
+        .ok()?
         .split_whitespace()
         .nth(1)
         .unwrap()
-        .parse::<u64>()?
+        .parse::<u64>()
+        .ok()?
         .saturating_mul(1_024);
     let free = lines
         .next()
-        .unwrap()?
+        .unwrap()
+        .ok()?
         .split_whitespace()
         .nth(1)
         .unwrap()
-        .parse::<u64>()?
+        .parse::<u64>()
+        .ok()?
         .saturating_mul(1_024);
     let buffers = lines
         .nth(1)
-        .unwrap()?
+        .unwrap()
+        .ok()?
         .split_whitespace()
         .nth(1)
         .unwrap()
-        .parse::<u64>()?
+        .parse::<u64>()
+        .ok()?
         .saturating_mul(1_024);
     let cached = lines
         .next()
-        .unwrap()?
+        .unwrap()
+        .ok()?
         .split_whitespace()
         .nth(1)
         .unwrap()
-        .parse::<u64>()?
+        .parse::<u64>()
+        .ok()?
         .saturating_mul(1_024);
     let s_reclaimable = lines
         .nth(20)
-        .unwrap()?
+        .unwrap()
+        .ok()?
         .split_whitespace()
         .nth(1)
         .unwrap()
-        .parse::<u64>()?
+        .parse::<u64>()
+        .ok()?
         .saturating_mul(1_024);
 
     let used = total - free - buffers - cached - s_reclaimable;
 
-    Ok(fmt_ram(used, total))
+    Some(fmt_ram(used, total))
 }
 
 fn fmt_ram(u: u64, t: u64) -> String {
@@ -178,7 +188,7 @@ fn fmt_ram(u: u64, t: u64) -> String {
     [bar, used].join(" ")
 }
 
-fn disk() -> Result<String> {
+fn disk() -> Option<String> {
     let mut total = 0;
     let mut available = 0;
 
@@ -200,13 +210,13 @@ fn disk() -> Result<String> {
     }
 
     if total == 0 {
-        bail!(UNKNOWN)
+        return None
     }
 
     let bar = disk_bar(available, total);
     let used = (((total - available) as f64 / 1_000_000_000f64) * 10f64).round() / 10f64;
 
-    Ok(format!("{} {:.1} \x1b[3;90mGB\x1b[0m", bar, used))
+    Some(format!("{} {:.1} \x1b[3;90mGB\x1b[0m", bar, used))
 }
 
 fn disk_bar(a: u64, t: u64) -> String {
@@ -233,24 +243,26 @@ fn get_bar(n: u8) -> String {
     r.to_string()
 }
 
-fn kernel() -> Result<String> {
+fn kernel() -> Option<String> {
     let mut ptr = std::mem::MaybeUninit::<libc::utsname>::zeroed();
     unsafe {
         match libc::uname(ptr.as_mut_ptr()) == 0 {
             true => {
                 let r = ptr.assume_init();
-                Ok(r.release
-                    .iter()
-                    .filter(|c| **c != 0)
-                    .map(|c| *c as u8 as char)
-                    .collect::<String>())
+                Some(
+                    r.release
+                        .iter()
+                        .filter(|c| **c != 0)
+                        .map(|c| *c as u8 as char)
+                        .collect::<String>()
+                )
             }
-            false => bail!(UNKNOWN)
+            false => None
         }
     }
 }
 
-fn pkgs() -> Result<(String, String)> {
+fn pkgs() -> Option<(String, String)> {
     let mut pkgs = read_to_string(
         [
             &var("XDG_CACHE_HOME").unwrap_or_else(|_| [&var("HOME").unwrap(), ".cache"].join("/")),
@@ -258,16 +270,17 @@ fn pkgs() -> Result<(String, String)> {
             "pkg_stats"
         ]
         .join("/")
-    )?;
+    )
+    .ok()?;
     pkgs.pop();
     let mut split = pkgs.split(' ');
-    Ok((split.next().unwrap().to_string(), split.next().unwrap().to_string()))
+    Some((split.next().unwrap().to_string(), split.next().unwrap().to_string()))
 }
 
-fn uptime() -> Result<String> {
-    let input = read_to_string("/proc/uptime")?;
+fn uptime() -> Option<String> {
+    let input = read_to_string("/proc/uptime").ok()?;
     let secs = input.split(' ').take(1).next().unwrap_or("");
-    let s = secs.parse::<f64>()?.round() as usize;
+    let s = secs.parse::<f64>().ok()?.round() as usize;
 
     let mut r = vec![];
 
@@ -295,5 +308,5 @@ fn uptime() -> Result<String> {
         false => r.push([(s % 60).to_string().as_str(), " seconds"].join(""))
     }
 
-    Ok(r.join(""))
+    Some(r.join(""))
 }
